4dea53ae9fd8892f098c8f1840238fbe1bec6dcd
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-11-09 03:10:48 +0700

    Add a more compact header

---
+++ src/AppHeader.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from 'react'
+import './AppHeader.css'
+import logo from './logo.svg'
+
+export default function AppHeader(props: { rightContent?: ReactNode }) {
+  return (
+    <div className="AppHeader">
+      <img src={logo} />
+      <h1>ppqr.app</h1>
+      <div className="AppHeaderのrightContent">{props.rightContent}</div>
+    </div>
+  )
+}

b52ac2a56805191deeb7e8d86c9e550538b83b98
fResult <Styxmaz@gmail.com>
2022-04-29 02:44:23 +0700

    Refactor QRCode component from js to ts

---
+++ src/QRCode.tsx
@@ -0,0 +1,62 @@
+import { Component, FunctionalComponent } from 'preact'
+import { useEffect, useRef, useState } from 'preact/hooks'
+import * as qr from 'qrcode'
+import createPixelsRenderer from './createPixelsRenderer'
+
+type QRCodeProps = {
+  payload: string | qr.QRCodeSegment[]
+}
+
+const QRCode: FunctionalComponent<QRCodeProps> = ({ payload }) => {
+  const qrCodeRef = useRef<HTMLDivElement>(null)
+  const rendererRef =
+    useRef<Nullable<ReturnType<typeof createPixelsRenderer>>>(null)
+  const payloadRef = useRef<Nullable<QRCodeProps['payload']>>(null)
+
+  useEffect(() => {
+    if (!rendererRef.current) {
+      rendererRef.current = createPixelsRenderer(qrCodeRef.current!)
+    }
+    update(payload)
+
+    return () => rendererRef.current?.dispose()
+  }, [payload])
+
+  function update(updatingPayload: QRCodeProps['payload']) {
+    payloadRef.current = payload
+
+    qr.toString(
+      payload,
+      { type: 'svg', errorCorrectionLevel: 'L' },
+      (err, svg) => {
+        if (err) {
+          window.alert('Cannot generate QR code: ' + String(err))
+          return
+        }
+        if (updatingPayload === payload) {
+          const sizeMatch = /width="(\d+)" height="(\d+)"/.exec(svg)
+          if (!sizeMatch) {
+            console.log(svg)
+            window.alert('Failed to parse SVG...')
+            return
+          }
+          const width = Number(sizeMatch[1]) / 4
+          const height = Number(sizeMatch[2]) / 4
+          const regexp = /x="(\d+)" y="(\d+)"/g
+          const pixels: { x: number; y: number }[] = []
+          for (;;) {
+            const m: RegExpExecArray = regexp.exec(svg)!
+            if (!m) break
+            const [_, x, y] = m
+            if (!+x && !+y) continue
+            pixels.push({ x: Number(x) / 4, y: Number(y) / 4 })
+          }
+          rendererRef.current?.update(width, height, pixels)
+        }
+      },
+    )
+  }
+  return <div className="qrcode" ref={qrCodeRef} />
+}
+
+export default QRCode
--- src/createPixelsRenderer.ts
+++ src/createPixelsRenderer.ts
@@ -39,8 +39,8 @@ export default function createPixelsRenderer(el: HTMLDivElement) {
     }
     drawMask(w)
     // TODO: `webkitMaskImage` is deprecated, maybe use `maskImage` instead
-    maskDiv.style.webkitMaskImage = `url("${maskCanvas.toDataURL()}")`
-    maskDiv.style.webkitMaskSize = `cover`
+    maskDiv.style.maskImage = `url("${maskCanvas.toDataURL()}")`
+    maskDiv.style.maskImage = `cover`
   }
 
   function drawMask(size: number) {

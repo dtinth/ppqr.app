2247b5aea58eb86aa67c308404c4dce045b2eb1e
dtinth on MBP M1 <dtinth@spacet.me>
2022-06-21 23:24:20 +0700

    Increase AppHeader size to 48px

--- src/AppHeader.tsx
+++ src/AppHeader.tsx
@@ -4,9 +4,9 @@ import logo from './logo.svg'
 
 export default function AppHeader(props: { rightContent?: Component }) {
   return (
-    <div className="AppHeader">
-      <img alt="" src={logo} />
-      <h1>ppqr.app</h1>
+    <div className="AppHeader h-12">
+      <img className="w-12 h-12" alt="" src={logo} />
+      <h1 className="font-bold">ppqr.app</h1>
       <div className="AppHeaderのrightContent">{props.rightContent}</div>
     </div>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { registerSW } from 'virtual:pwa-register'
 
 import App from './App'
 
+import '@fontsource/noto-sans-thai'
+import '@fontsource/roboto'
+import '@fontsource/roboto/700.css'
 import './index.css'
 
 render(<App />, document.getElementById('app')!)
